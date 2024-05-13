ea4e9b013520eeb43da972312222f780af9b394d
SharrpY <toprakgulen@hotmail.com>
2024-05-13 15:14:01 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <LibraryBooksIcon />
+        KeepIt
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
